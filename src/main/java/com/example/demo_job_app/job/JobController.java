fadd49eb6b275766b47be7f034ed0bf2f242f953
Andreas Panagiotopoulos <panagandrea@gmail.com>
2026-01-26 20:37:13 +0200

    Request mapping annotation added

--- src/main/java/com/example/demo_job_app/job/JobController.java
+++ src/main/java/com/example/demo_job_app/job/JobController.java
@@ -10,6 +10,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+//Request mapping annotation {alternative way. two parameters, 1st parameter::value --> specify the path
+//                                                             2nd parameter::method-->request type}
+//When request mapping is applied in a class level, it sets a base URL path for all the methods that are handling request in that particular controller
+@RequestMapping("/jobs")
 public class JobController {
 
     private JobService jobService;
@@ -18,19 +22,24 @@ public class JobController {
         this.jobService = jobService;
     }
 
-    @GetMapping("/jobs")
+    /*    Request mapping annotation helps to reduce repetition, in the future if you want to change the base URL,
+        you just   have to change in one place and is applied for all the controller rather than going in every request(method) and change it individually */
+    //    @GetMapping("/jobs")
+    @GetMapping
     public ResponseEntity<List<Job>> findAll() {
         return ResponseEntity.ok(jobService.findAll());
     }
 
-    @PostMapping("/jobs")
+    //    @PostMapping("/jobs")
+    @PostMapping
     public ResponseEntity<String> createJob(@RequestBody Job job) {
         jobService.createJob(job);
 //        return new ResponseEntity<>("Job created successfully",  HttpStatus.CREATED);
         return new ResponseEntity<>("Job created successfully", HttpStatus.CREATED);
     }
 
-    @GetMapping("/jobs/{id}")
+    //    @GetMapping("/jobs/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<Job> getJobById(@PathVariable Long id) {
 
         Job job = jobService.getJobById(id);
@@ -39,7 +48,8 @@ public class JobController {
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
-    @DeleteMapping("/jobs/{id}")
+    //    @DeleteMapping("/jobs/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteJob(@PathVariable Long id) {
         boolean deleted = jobService.deleteJobById(id);
         if (deleted)
@@ -47,7 +57,9 @@ public class JobController {
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
-    @PutMapping("/jobs/{id}")
+    //    @PutMapping("/jobs/{id}")
+    @PutMapping("/{id}")
+    //@RequestMapping(value = "/jobs/{id}", method = RequestMethod.PUT)
     public ResponseEntity<String> updateJob(@PathVariable Long id, @RequestBody Job updatedJob) {
         //updateJob method parameters: id-->which job to update
         //                             updateJob-->what to update
