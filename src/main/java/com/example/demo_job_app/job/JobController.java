8fe33c0eb6c92f79e700ae0545b7db95b7f498e4
Andreas Panagiotopoulos <panagandrea@gmail.com>
2026-01-26 18:57:02 +0200

    Update the controller for all methods to return response entity

--- src/main/java/com/example/demo_job_app/job/JobController.java
+++ src/main/java/com/example/demo_job_app/job/JobController.java
@@ -19,14 +19,15 @@ public class JobController {
     }
 
     @GetMapping("/jobs")
-    public List<Job> findAll() {
-        return jobService.findAll();
+    public ResponseEntity<List<Job>> findAll() {
+        return ResponseEntity.ok(jobService.findAll());
     }
 
     @PostMapping("/jobs")
-    public String createJob(@RequestBody Job job) {
+    public ResponseEntity<String> createJob(@RequestBody Job job) {
         jobService.createJob(job);
-        return "Job added successfully";
+//        return new ResponseEntity<>("Job created successfully",  HttpStatus.CREATED);
+        return new ResponseEntity<>("Job created successfully", HttpStatus.OK);
     }
 
     @GetMapping("/jobs/{id}")
